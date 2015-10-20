eb5b2744c7e8a3ac18176753b04a8494999243bc
Anton Gärdälv <anton@gardalv.se>
2015-10-21 01:27:14 +0200

    Functionality

---
+++ Source/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Apps\Store\Source\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Apps\Store\Source\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ Source/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Apps\Store\Source\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Apps\Store\Source\platforms\android\AndroidManifest.xml \
